a31673e9e8a154e85be885244a487977c13c47be
João M. Campagnolo <jota.campagnolo@gmail.com>
2017-09-26 18:13:01 -0300

    Adicionadas as páginas de Login e Signup. Alteração de página ativa incluida na função printMenu().

--- source/layout_functions.php
+++ source/layout_functions.php
@@ -1,7 +1,7 @@
 <?php
 function printBanner(){
     echo '<div class="jumbotron text-center" style="border-radius: 0px; background-image: url(images/layout/banner.png); background-position: center center; margin-bottom: 0px">
-        <h1 style="color: white">Medivia Porfile</h1>
+        <h1 style="color: white">Medivia Profile</h1>
     </div>';
 }
 
@@ -23,18 +23,31 @@ function printMenu($pageName){
                 <ul class="nav navbar-nav">';
                     // Menu: NEWS:
                     if($pageName == "index" || $pageName == "news"){
-                        echo '<li class="active"><a href="#">News<span class="sr-only">(current)</span></a></li>';
+                        echo '<li class="active"><a href="index.php">News<span class="sr-only">(current)</span></a></li>';
                     }else{
-                        echo '<li><a href="#">News</a></li>';
+                        echo '<li><a href="index.php">News</a></li>';
                     }
                 echo '</ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="fa fa-user-circle-o" style="margin-right: 8px"></span>Account<span class="caret" style="margin-left: 8px"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><span class="fa fa-sign-in" style="margin-right: 8px"></span>Login</a></li>
-                            <li><a href="#"><span class="fa fa-plus" style="margin-right: 8px"></span>Create Account</a></li>
-                        </ul>
+                <ul class="nav navbar-nav navbar-right">';
+                    // Menu: Account:
+                    if($pageName == "user_signup_page" || $pageName == "user_login_page"){
+                        echo '<li class="dropdown active">';
+                    }else{
+                        echo '<li class="dropdown">';
+                    }
+                        echo '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="fa fa-user-circle-o" style="margin-right: 8px"></span>Account<span class="caret" style="margin-left: 8px"></span></a>
+                        <ul class="dropdown-menu">';
+                            if($pageName == "user_login_page"){
+                                echo '<li class="active"><a href="user_login_page.php"><span class="fa fa-sign-in" style="margin-right: 8px"></span>Login</a></li>';
+                            }else{
+                                echo '<li><a href="user_login_page.php"><span class="fa fa-sign-in" style="margin-right: 8px"></span>Login</a></li>';
+                            }
+                            if($pageName == "user_signup_page"){
+                                echo '<li class="active"><a href="user_signup_page.php"><span class="fa fa-plus" style="margin-right: 8px"></span>Create Account</a></li>';
+                            }else{
+                                echo '<li><a href="user_signup_page.php"><span class="fa fa-plus" style="margin-right: 8px"></span>Create Account</a></li>';
+                            }
+                        echo '</ul>
                     </li>
                 </ul>
             </div><!-- /.navbar-collapse -->
--- source/user_signup_page.php
+++ source/user_signup_page.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>News</title>
+        <title>Sign Up</title>
 
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -30,7 +30,7 @@
             <div class="row"> <!-- Page Header and Menu -->
                 <?php
                     printBanner();
-                    printMenu("index");
+                    printMenu("user_signup_page");
                 ?>
             </div>
             <div class="row">
